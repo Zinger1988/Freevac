7223505ff5b261c4f4a5937bf10dba75bc03d8be
Serhii Honchar <sergeygonchar88@gmail.com>
2021-08-09 13:42:54 +0300

    WIP

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -68,5 +68,80 @@ const SiteJS = {
     init: function () {
         // this.inputFocus();
         new InputFocus('.input-row');
+        this.moveElement({
+            elementId: 'user-title',
+            targetId: 'profile-video',
+            mediaQuery: 'max-width: 991px',
+            insertionMethod: function (element, target) {
+                target.prepend(element);
+            }
+        });
+        this.moveElement({
+            elementId: 'user-description',
+            targetId: 'user-position',
+            mediaQuery: 'max-width: 767px',
+            insertionMethod: function (element, target) {
+                target.after(element);
+            }
+        });
+        this.moveElement({
+            elementId: 'profile-view-primary',
+            targetId: 'profile-view-grid',
+            mediaQuery: 'max-width: 991px',
+            insertionMethod: function (element, target) {
+                target.prepend(element);
+            }
+        });
+        this.moveElement({
+            elementId: 'take-video-balloon',
+            targetId: 'profile-video',
+            mediaQuery: 'max-width: 991px',
+            insertionMethod: function (element, target) {
+                target.prepend(element);
+            }
+        });
+        this.moveElement({
+            elementId: 'user-info',
+            targetId: 'profile-video',
+            mediaQuery: 'max-width: 991px',
+            insertionMethod: function (element, target) {
+                target.prepend(element);
+            }
+        });
+    },
+    resizeThrottler: function(cb){
+        let throttle = false;
+
+        window.addEventListener('resize', () => {
+            if(!throttle) {
+                throttle = true;
+                setTimeout(() => {
+                    cb();
+                    throttle = false;
+                }, 500)
+            }
+        })
+    },
+    moveElement: function ({elementId, targetId, mediaQuery, insertionMethod}) {
+        const element = document.getElementById(elementId);
+        const target = document.getElementById(targetId);
+
+        if(!element || !target) return;
+
+        const elementMockup = document.createElement('div');
+        elementMockup.cssText = "display: none";
+        elementMockup.setAttribute('data-element-dom-anchor',`${elementId}`);
+        element.after(elementMockup);
+
+        move();
+        this.resizeThrottler(move);
+
+        function move() {
+            if(window.matchMedia(`(${mediaQuery})`).matches){
+                insertionMethod(element, target);
+            } else {
+                elementMockup.before(element);
+            }
+        }
     }
 };
